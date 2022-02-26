72f6583760d67843cd6a4a3652a7e0d7c4bd8107
Arman B <81931943+iryston-hash@users.noreply.github.com>
2022-02-26 19:21:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+$(function(){
+    $('.carousel__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1,
+                    dots: false,
+                }
+            },
+        ]
+    });
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1600) {
+          $('.pageup').fadeIn();
+        } else {
+          $('.pageup').fadeOut();
+        }
+    });
+    window.addEventListener('load', AOS.refresh);
+});
